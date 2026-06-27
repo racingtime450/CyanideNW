a50ff4b8e146c929a968841475f482c97c4e5dee
racingtime450 <racingtime154@gmail.com>
2026-06-27 22:55:25 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+{
+  "name": "cyanidenw",
+  "version": "1.0.0",
+  "description": "CyanideNW Minecraft server website with PayTR",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.21.2"
+  },
+  "engines": {
+    "node": ">=18"
+  }
+}
